c3b390a7cb09b201f95270ecc1eef26a1c4f310b
Saad Aswad <132266156+LeadstarlingX@users.noreply.github.com>
2026-05-22 01:33:03 +0300

    Integration tests preparations

--- GridTrack.IntegrationTests/Abstractions/BaseIntegrationTest.cs
+++ GridTrack.IntegrationTests/Abstractions/BaseIntegrationTest.cs
@@ -7,29 +7,10 @@ namespace GridTrack.IntegrationTests.Abstractions;
 
 public abstract class BaseIntegrationTest
 {
-    protected IntegrationTestWebAppFactory factory = null!;
-    protected IServiceScope _scope = null!;
-    protected IMessageBus Sender = null!;
     
     [ClassDataSource<IntegrationTestWebAppFactory>(Shared = SharedType.PerAssembly)]
     public static IntegrationTestWebAppFactory Factory { get; set; } = null!;
     
-    [Before(Test)]
-    public async Task SetupTest()
-    {
-        factory = Factory;
 
-        _scope = factory.Services.CreateScope();
-        Sender = _scope.ServiceProvider.GetRequiredService<IMessageBus>();
-    }
-
-    [After(Test)]
-    public async ValueTask TeardownTest()
-    {
-        if (_scope is IAsyncDisposable asyncScope)
-            await asyncScope.DisposeAsync();
-        else
-            _scope.Dispose();
-    }
     
 }
