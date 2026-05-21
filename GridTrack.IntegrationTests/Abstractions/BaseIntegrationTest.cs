c8232bf7101fe6db9794240abe191819db787f35
Saad Aswad <132266156+LeadstarlingX@users.noreply.github.com>
2026-05-22 00:42:54 +0300

    TestContainers ready

---
+++ GridTrack.IntegrationTests/Abstractions/BaseIntegrationTest.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace GridTrack.IntegrationTests.Abstractions;
+
+public abstract class BaseIntegrationTest
+{
+    
+    [ClassDataSource<IntegrationTestWebAppFactory>(Shared = SharedType.PerAssembly)]
+    public static IntegrationTestWebAppFactory factory { get; set; }
+    
+}
---
+++ GridTrack.IntegrationTests/Abstractions/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,67 @@
+﻿using GridTrack.Api;
+using GridTrack.Application.Abstractions.Clock;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using NSubstitute;
+using Testcontainers.MsSql;
+using Testcontainers.Redis;
+using TUnit.Core.Interfaces;
+
+namespace GridTrack.IntegrationTests.Abstractions;
+
+public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncInitializer
+{
+    // Obsolete constructors will be removed in future versions, keep the parameter constructor
+    //  it's enough to pass the image.
+    
+    public readonly IDateTimeProvider DateTimeProviderMock = Substitute.For<IDateTimeProvider>();
+
+    private readonly MsSqlContainer _dbContainer =
+        new MsSqlBuilder("postgis/postgis:18-3.6")
+            .WithPassword("postgres")
+            .Build();
+
+
+    private readonly RedisContainer _redisContainer =
+        new RedisBuilder("redis:8.4.0")
+            .Build();
+
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+        await _redisContainer.StartAsync();
+
+        // Force the host to start and apply migrations before any tests run
+        using var _ = CreateClient();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {   
+        
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(IDateTimeProvider));
+            
+            services.AddScoped<IDateTimeProvider>(_ =>
+            {
+                var mock = Substitute.For<IDateTimeProvider>();
+                mock.UtcNow.Returns(_ => DateTime.UtcNow);
+                return mock;
+            });
+        });
+        
+        Environment.SetEnvironmentVariable("ConnectionStrings:DefaultConnection", _dbContainer.GetConnectionString());
+        Environment.SetEnvironmentVariable("ConnectionStrings:Redis", _redisContainer.GetConnectionString());
+        
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+        await _redisContainer.StopAsync();
+    }
+}
